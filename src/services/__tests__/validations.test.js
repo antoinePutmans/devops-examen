7eac497957a31538986339f4d3c541d0baa9f1cb
antoinePutmans <antoine.putmans@student.vinci.be>
2023-01-20 10:23:44 +0100

    test: adding 8 char gamertag tests

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,4 +1,4 @@
-const { isEmpty } = require("../validations");
+const { isEmpty,is8CharMin } = require("../validations");
 
 describe("validations tests suites - isEmpty", () => {
     test("should return true as the label is undefined", () => {
@@ -15,6 +15,28 @@ describe("validations tests suites - isEmpty", () => {
         const result = isEmpty("Label");
         expect(result).toBe(false);
     });
+
+    
 });
 
 // TODO: Create tests suite for validation function
+
+describe("validations tests suites - is8CharMin", () => {
+    
+    test("should return false as the gamertag length is < 8", () => {
+        const result = is8CharMin("Label");
+        expect(result).toBe(false);
+    });
+
+    test("should return true as the gamertag length is === 8", () => {
+        const result = is8CharMin("gamertag");
+        expect(result).toBe(true);
+    });
+
+    test("should return true as the gamertag length is > 8", () => {
+        const result = is8CharMin("dlstechteaching");
+        expect(result).toBe(true);
+    });
+
+    
+});
--- src/services/validations.js
+++ src/services/validations.js
@@ -2,4 +2,7 @@
 
 const isEmpty = (label) => !label || label.length === 0;
 
+const is8CharMin = (label) => label.length >= 8 ;
+
 exports.isEmpty = isEmpty;
+exports.is8CharMin = is8CharMin;
