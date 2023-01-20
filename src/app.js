c1904e914ef7ca71acf707e6381ecf6be71a6715
antoinePutmans <antoine.putmans@student.vinci.be>
2023-01-20 10:50:52 +0100

    feat: adding check message if tag invalide

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 try {
-    const { isEmpty } = require("./services/validations");
+    const { isEmpty, is8CharMin, containsSpecialsChar, containsNumbers } = require("./services/validations");
     const gamerTagInput = document.getElementById("gamerTagInput");
     const checkButton = document.getElementById("gamerTagCheckButton");
     const feedbackMessage = document.getElementById("gamerTagFeedback");
@@ -14,7 +14,7 @@ try {
 
     checkButton.addEventListener("click", () => {
         // TODO: Add the logic to display the correct feedback message (error and validation)
-        feedbackMessageText = isEmpty(gamerTagValue)
+        feedbackMessageText = (isEmpty(gamerTagValue) && is8CharMin(gamerTagInput) && containsSpecialsChar(gamerTagInput) && containsNumbers(gamerTagInput))
             ? "Gamer tag cannot be empty"
             : "Gamer tag is valid";
         feedbackMessage.textContent = feedbackMessageText;
