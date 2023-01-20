c0db03a552495325ebc067a3078231c42eaf661c
antoinePutmans <antoine.putmans@student.vinci.be>
2023-01-20 10:41:15 +0100

    test: adding test2

--- src/services/validations.js
+++ src/services/validations.js
@@ -4,5 +4,16 @@ const isEmpty = (label) => !label || label.length === 0;
 
 const is8CharMin = (label) => label.length >= 8;
 
+const containsSpecialsChar = (label) => {
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    if (format.test(label)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
 exports.isEmpty = isEmpty;
 exports.is8CharMin = is8CharMin;
+exports.containsSpecialsChar = containsSpecialsChar;
